649230731bfa96369125c0ebe0da754264b9184a
Matic Lukežič <maticlukezic007@gmail.com>
2023-12-06 20:59:06 +0100

    Add logo asset and write classes needed to load it into the game

---
+++ core/src/com/sudokugame/game/assets/AssetPaths.java
@@ -0,0 +1,10 @@
+package com.sudokugame.game.assets;
+
+public class AssetPaths {
+
+    public static final String GAME_PLAY = "gameplay/gameplay.atlas";
+    public static final String SKIN_JSON = "skin/sgx-ui.json";
+    public static final String SKIN_ATLAS = "skin/sgx-ui.atlas";
+    private AssetPaths() {
+    }
+}
---
+++ core/src/com/sudokugame/game/assets/RegionNames.java
@@ -0,0 +1,10 @@
+package com.sudokugame.game.assets;
+
+public class RegionNames {
+
+    public static final String LOGO = "logo";
+    public static final String LOGO2 = "logo2";
+    public static final String LOGO50 = "logo50";
+    private RegionNames() {
+    }
+}
